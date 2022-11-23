5f3fe4734ba3d721e2f48b58c1252e1a5f43d8e8
ashiba <ashibasama@gmail.com>
2022-11-23 11:21:37 +0900

    reset.css

--- sns_1.php
+++ sns_1.php
@@ -4,9 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/earlyaccess/hannari.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>SNS_1</title>
 </head>
 <body>
+    <h1>– 掲示板サイトへようこそ –</h1>
     <form action="sns_2.php" method="post">
         名前
         <div>
@@ -27,7 +31,7 @@
         $db = new PDO("mysql:host=localhost;dbname=db", "root", "root");
         $ps = $db->query("SELECT * FROM tab ORDER BY ban DESC");
         while ($r = $ps->fetch()) {
-            print "{$r['ban']} {$r['nam']} {$r['dat']}<br>" .nl2br($r['mes'])."<hr>";
+            print "{$r['ban']} {$r['nam']} {$r['dat']}<br>".nl2br($r['mes'])."<hr>";
         }
     ?>
 </body>
