6d0e06eaac0f887009a32501b305d48317017d13
ashiba <ashibasama@gmail.com>
2022-11-23 10:24:21 +0900

    next css

--- sns_2.php
+++ sns_2.php
@@ -10,10 +10,10 @@
     <?php
     $my_nam = htmlspecialchars($_POST['n'], ENT_QUOTES);
     $my_mes = htmlspecialchars($_POST['m'], ENT_QUOTES);
-    $db = new PDO("mysql:host = localhost; dbname = db", "root", "root");
+    $db = new PDO("mysql:host=localhost;dbname=db", "root", "root");
     $db->query("INSERT INTO tab (ban, nam, mes, dat) VALUES(NULL, '$my_nam', '$my_mes', NOW())");
     print "書き込みに成功！";
-    "<p><a href='sns_1.php'>一覧表示へ</a></p>";
+    print"<p><a href='sns_1.php'>一覧表示へ</a></p>";
     ?>
 </body>
 </html>
--- sns_3.php
+++ sns_3.php
@@ -9,13 +9,13 @@
 <body>
     <?php
     $my_sea = htmlspecialchars($_POST["s"], ENT_QUOTES);
-    $db = new PDO("mysql:host = localhost; dbname = db", "root", "root");
+    $db = new PDO("mysql:host=localhost;dbname=db", "root", "root");
     print "<p style='font-size: 20pt'>「{$my_sea}」の検索結果</p>";
     $ps = $db->query("SELECT * FROM tab WHERE mes like '%$my_sea%'");
     while ($r = $ps->fetch()) {
         print "{$r['ban']} {$r['nam']} {$r['dat']}<br>" .nl2br($r['mes'])."<hr>";
     }
-        print "<p><a href='sns1.php'>一覧表示へ</a></p>";
+        print "<p><a href='sns_1.php'>一覧表示へ</a></p>";
     ?>
 </body>
 </html>
